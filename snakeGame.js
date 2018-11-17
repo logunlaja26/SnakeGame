fa52ed487a347d8558ec028d0d78e422b390887b
logunlaja26 <logunlaja26@gmail.com>
2018-11-17 14:03:27 -0600

    movement refactoring

--- snakeGame.js
+++ snakeGame.js
@@ -16,19 +16,35 @@ window.onload = function() {
 		{x: 110, y: 150}
 	]
 
-	setInterval(function(){main(snake);},1000);	
-	//snake.forEach(drawSnakePart)
+	setInterval(function(){main(snake);},1000 );	
+
+	function main(snake){
+	drawCanvas();
+	drawFood();
+
+	let snakeCopy = [];
+
+	snake.forEach(function(snakePart){
+		snakeCopy.push(snakePart);
+	});
+
+	snake[0] = {x: snakeCopy[0].x, y: snakeCopy[0].y - 10};
+	snake[1] = {x: snakeCopy[0].x, y: snakeCopy[0].y};
+	snake[2] = {x: snakeCopy[1].x, y: snakeCopy[1].y};
+	snake[3] = {x: snakeCopy[2].x, y: snakeCopy[2].y};
+	snake[4] = {x: snakeCopy[3].x, y: snakeCopy[3].y};
+
+	drawSnakePart(snake[0]);
+	drawSnakePart(snake[1]);
+	drawSnakePart(snake[2]);
+	drawSnakePart(snake[3]);
+	drawSnakePart(snake[4]);
+}
+	
 
 }
 
 function drawSnakePart(snakePart){
-	function moveUp(snakePart){
-		snakePart.y = snakePart.y - 10;
-		return snakePart;
-	}
-
-	snakePart = moveUp(snakePart)
-
 	let canvas = document.getElementById('gameCanvas');
 	let canvasBackground = canvas.getContext('2d');
 	canvasBackground.fillStyle = 'blue';
@@ -52,15 +68,15 @@ function drawFood(){
 	canvasBackground.fillRect(250,250,15,15);
 }
 
-function main(snake){
-	drawCanvas();
-	drawFood();
+function snakeBody(){
 	drawSnakePart(snake[0]);
 	drawSnakePart(snake[1]);
 	drawSnakePart(snake[2]);
 	drawSnakePart(snake[3]);
 	drawSnakePart(snake[4]);
 }
+
+
  	
 
  	
